58fcc79c086834a51195a313dd7f7ebfa7b3531c
MAngelozz <73148583+MAngelozz@users.noreply.github.com>
2022-08-21 18:24:22 -0300

    Delete proyectoPythonSec.py.py

--- proyectoPythonSec.py.py
+++
@@ -1,9 +0,0 @@
-
-
-import socket
-
-hostname = socket.gethostname()
-ip = socket.gethostbyname (hostname)
-
-print ("El nombre de tu ordenador es: " + hostname)
-print ("tu disreccion IP es " + ip)
